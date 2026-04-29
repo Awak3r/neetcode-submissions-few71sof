26a447b462942dca70c1de43937c8d8883fc3025
Mikhail Bogdanov <bogdanovmihail129@gmail.com>
2026-04-29 19:37:49 +0300

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int s_len = s1.size();
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
+        unordered_set<char> ss;
+        for(int i = 0; i < s_len; i++){
+            m1[s1[i]]++;
+            m2[s1[i]] = 0;
+            ss.insert(s1[i]);
+        }
+        int cur_len = 0;
+        int l = 0;
+        for(int i = 0; i < s2.size(); i++){
+            if (ss.find(s2[i]) == ss.end()){
+                l = i + 1;
+                cur_len = 0;
+                for (auto& [key, value] : m2) {
+                    value = 0;
+                }
+                continue;
+            }
+            m2[s2[i]]++;
+            cur_len++;
+            while (cur_len > s_len){
+                m2[s2[l]]--;
+                cur_len--;
+            }
+            if(m1 == m2) return true;
+        }        
+        return false;
+    }
+};
